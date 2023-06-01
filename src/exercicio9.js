ef6aa2e170d304c88730b995a455e3a4217b2ab1
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-05-31 21:23:26 -0300

    first commit

---
+++ src/exercicio9.js
@@ -0,0 +1,11 @@
+function somar(a, b) {
+    return [a,b]
+    /*let v = []; //cria um array vazio
+    v[0] = a;
+    v[1] = b;
+    return v;*/
+  }
+  let a = [5, 8, 3, 4, 9, 7];
+  let b = [3, 5, 9, 2, 3, 4];
+  let r = somar(a, b);
+  console.log("JUntar:", r);
